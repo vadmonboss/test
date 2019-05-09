0b046a74f7f6d9637e173558697df10f3a493b40
Vadim <xgtamen@gmail.com>
2019-05-10 00:25:00 +0300

    это наш

---
+++ test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+let something;
+console.log(4 / 0);
+console.log('string' * 9);
+console.log(something);
+
+//let person = {
+//    name: "John",
+//  age: 25,
+//  isMerried: false 
+//};
+//console.log(person["name"]);
+
+//let arr = ['plum.png','orange.png','apple.bmp']
+//console.log(arr[0])
+
+//alert('Hello world');
+
+//let anser = prompt('your mam?', 'Да');
+//console.log(anser); 
+//console.log(typeof(arr)); 
+
+//console.log(4 + " - object");
+
+
+
+//var mamka = 0;
+//while (mamka<=99) {
+// document.write(mamka + "<br/>");
+/// mamka++;
+//}
+
+
+let incr = 10,
+  decr = 10;
+incr++;
+decr--;
+console.log(incr++);
+console.log(decr--);
+document.write(5 % 2, "<br/>");
+document.write("2" === 2);
+
+let isCheck = true,
+    isClose = false;
+console.log(isCheck || isClose);
